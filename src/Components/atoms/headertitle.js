285596aadfc97fa6783e3a77d4f21082aa00a120
veerleprins <veerleprins@hotmail.com>
2020-11-26 11:26:05 +0100

    Updated for deploying

--- src/Components/atoms/headertitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const HeaderTitle = () => {
-  return (
-    <>
-      <h1> DE <span className="greenColor">ELEKTRISCHE AUTO</span> IN NEDERLAND</h1>
-    </>
-  )
-};
--- src/Components/atoms/subheading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SubHeading = ({ subtitle }) => {
-  return <>
-    <h2 className="sub-title">{ subtitle }</h2>
-  </>
-};
